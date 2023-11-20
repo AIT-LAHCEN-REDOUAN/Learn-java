cda2312e65ead17fd666b5dbd590dcd065d3a5e9
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2023-11-20 08:01:53 -0800

    add Code

--- Learn_java/src/Exercices/Ex1.java
+++ Learn_java/src/Exercices/Ex1.java
@@ -18,5 +18,36 @@ public class Ex1 {
         int a1 = 3 ;
         int b1 = 50 ;
         System.out.println("Solution is : "+(float)(3/50));
+
+        //Ex4
+
+        System.out.println("a : "+(-5+8*6));
+        System.out.println("b : "+(55+9)%9);
+        System.out.println("c : "+(20+(-3*5)/8));
+        System.out.println("d : "+(5+(15/3*2)-(8%3)));
+
+        //Ex5
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the First Number : ");
+        int num1 = input.nextInt();
+        System.out.println("Enter the Second Number : ");
+        int num2 = input.nextInt();
+
+        System.out.println("the Square Of "+num1+" and "+num2+" is : "+(num1*num2));
+        System.out.println("the Addition Of "+num1+" and "+num2+" is : "+(num1+num2));
+        System.out.println("the SubStract Of "+num1+" and "+num2+" is : "+(num1-num2));
+        System.out.println("the Division Of "+num1+" and "+num2+" is : "+(num1/num2));
+        System.out.println("the Mod Of "+num1+" and "+num2+" is : "+(num1%num2));
+
+        //Ex7
+
+        for (int i = 0 ; i<=num2;i++){
+            System.out.println(num1*i);
+        }
+
+
+
+
     }
 }
