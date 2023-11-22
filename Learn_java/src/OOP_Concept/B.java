93ded932786e1cc94eef61f080cda1567e37e438
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2023-11-22 13:40:23 -0800

    add code

--- Learn_java/src/OOP_Concept/B.java
+++ Learn_java/src/OOP_Concept/B.java
@@ -3,6 +3,5 @@ import OOP_Concept.* ;
 public class B {
     public static void main(String[] Args ){
         A NumberA = new A(5,10);
-        System.out.println(NumberA.display());
     }
 }
---
+++ Learn_java/src/OOP_Concept/Constructors.java
@@ -0,0 +1,13 @@
+package OOP_Concept;
+
+public class Constructors {
+
+    Constructors(){
+        super();
+        System.out.println("welcome");
+    }
+
+    public static void main(String[] Args){
+        Constructors C = new Constructors();
+    }
+}
