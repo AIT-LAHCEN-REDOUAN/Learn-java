77034ac243b099cd2164dfbb9a14673f86d947ea
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2023-12-25 14:40:10 -0800

    add Code

---
+++ Learn_java/src/JDBC_TUTORIAL/Example.java
@@ -0,0 +1,30 @@
+package JDBC_TUTORIAL;
+
+import java.sql.*;
+
+public class Example {
+    static final String DB_URL = "jdbc:mysql://localhost:3306/pospay";
+    static final String user = "root";
+    static final String password = "";
+
+    static final String Query = "SELECT * FROM utilisateurs";
+
+
+    public static void main(String[] Args){
+        try(
+                Connection conn = DriverManager.getConnection(DB_URL,user,password);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(Query);
+
+        ){
+            while (rs.next()){
+                System.out.println("id : "+rs.getInt("id"));
+                System.out.println("username : "+rs.getString("username"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();;
+        }
+    }
+
+
+}
