54c7428a06a9332d5b615d38a82063ff638d3d3e
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2023-12-27 02:55:59 -0800

    Start JAVA Swing GUI With JDBC

--- Learn_java/src/University_system/Splash.java
+++ Learn_java/src/University_system/Splash.java
@@ -1,17 +1,53 @@
 package University_system;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Splash extends JFrame {
+public class Splash extends JFrame implements Runnable {
 
 
+    Thread t ;
     Splash(){
 
-        setLocation(250,50);
-        setSize(1000,700);
+        ImageIcon i1 = new ImageIcon(getClass().getResource("/University_system/icons/first.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1000,700,Image.SCALE_DEFAULT);
+        ImageIcon i3  = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+
+        t = new Thread(this);
+
+        t.start();
         setVisible(true);
+        int x= 1 ;
+        for (int i =2 ; i <= 600 ; i+=4 , x+=1 ){
+            setLocation(600-((i+4)/2),350-(i/2));
+            setSize(i+3*x,i+x/2);
+        }
+
+        try {
+            Thread.sleep(10);
+        }catch (Exception e){
+
+        }
+
+
 
     }
+
+
+    public void run(){
+        try {
+            Thread.sleep(7000);
+            setVisible(false);
+
+
+            //Next Frame
+        } catch (Exception e){
+
+        }
+    }
+
     public static void main(String[] Args){
         Splash s = new Splash();
     }
