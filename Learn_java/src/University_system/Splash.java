3a8075fd803359546556590f62d83a5970d21aa9
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2023-12-27 02:27:39 -0800

    New Project

---
+++ Learn_java/src/University_system/Splash.java
@@ -0,0 +1,18 @@
+package University_system;
+
+import javax.swing.*;
+
+public class Splash extends JFrame {
+
+
+    Splash(){
+
+        setLocation(250,50);
+        setSize(1000,700);
+        setVisible(true);
+
+    }
+    public static void main(String[] Args){
+        Splash s = new Splash();
+    }
+}
