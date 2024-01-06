5aa3667ba900702653df0ce83aeced3b40399576
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-01-06 05:51:06 -0800

    Add Inputs

--- Learn_java/src/University_system/AddStudent.java
+++ Learn_java/src/University_system/AddStudent.java
@@ -2,28 +2,62 @@ package University_system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class AddStudent extends JFrame {
 
+    JTextField tfname ,tffname ;
+    JLabel labelfrollno ;
+
+    Random ran = new Random();
+    long first4 = Math.abs((ran.nextLong() % 9000L)+1000L);
     AddStudent(){
         setSize(900,700);
         setLocation(350,50);
 
         setLayout(null);
         JLabel heading = new JLabel("New Student Information");
-        heading.setBounds(310,30,500,50);
+        heading.setBounds(280,30,500,50);
         heading.setFont(new Font("serif",Font.BOLD,30));
         add(heading);
 
         JLabel lblname = new JLabel("Name");
-        lblname.setBounds(310,150,100,50);
+        lblname.setBounds(100,150,100,50);
         lblname.setFont(new Font("serif",Font.BOLD,20));
         add(lblname);
+
+        tfname  = new JTextField();
+        tfname.setBounds(200,160,150,30);
+        add(tfname);
+
+        JLabel lblfname = new JLabel("Father's Name");
+        lblfname.setBounds(400,150,200,50);
+        lblfname.setFont(new Font("serif",Font.BOLD,20));
+        add(lblfname);
+
+        tfname  = new JTextField();
+        tfname.setBounds(600,160,150,30);
+        add(tfname);
+
+        JLabel lblfrollno = new JLabel("Role Number : ");
+        lblfrollno.setBounds(50,230,200,50);
+        lblfrollno.setFont(new Font("serif",Font.BOLD,20));
+        add(lblfrollno);
+
+        labelfrollno = new JLabel("1533"+first4);
+        labelfrollno.setBounds(200,230,200,50);
+        labelfrollno.setFont(new Font("serif",Font.BOLD,20));
+        add(labelfrollno);
+
+        JLabel lbldob = new JLabel("Date Of Birth ");
+        lbldob.setBounds(400,230,200,50);
+        lbldob.setFont(new Font("serif",Font.BOLD,20));
+        add(lbldob);
 
 
         setVisible(true);
     }
-   public static void main(String Args){
-       new AddStudent();
+   public static void main(String[] Args){
+        new AddStudent();
    }
 }
