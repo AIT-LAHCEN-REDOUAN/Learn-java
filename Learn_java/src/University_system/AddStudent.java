46e96648ba0158ebe6f4adc9e53096f1669319dc
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-01-07 08:07:47 -0800

    Add CompoBox

--- Learn_java/src/University_system/AddStudent.java
+++ Learn_java/src/University_system/AddStudent.java
@@ -3,11 +3,15 @@ package University_system;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import com.toedter.calendar.*;
 
 public class AddStudent extends JFrame {
 
-    JTextField tfname ,tffname ;
+    JTextField tfname ,tfphone , tfaddress , tfemail , tfx , tfx1 , tfSN ;
     JLabel labelfrollno ;
+    JDateChooser dcdob ;
+
+    JComboBox cbcourse ;
 
     Random ran = new Random();
     long first4 = Math.abs((ran.nextLong() % 9000L)+1000L);
@@ -53,6 +57,77 @@ public class AddStudent extends JFrame {
         lbldob.setBounds(400,230,200,50);
         lbldob.setFont(new Font("serif",Font.BOLD,20));
         add(lbldob);
+
+        dcdob = new JDateChooser();
+        dcdob.setBounds(600,240,150,30);
+        add(dcdob);
+
+        JLabel lblfadress = new JLabel("Address :");
+        lblfadress.setBounds(100,300,200,50);
+        lblfadress.setFont(new Font("serif",Font.BOLD,20));
+        add(lblfadress);
+
+        tfaddress  = new JTextField();
+        tfaddress.setBounds(200,310,150,30);
+        add(tfaddress);
+
+        JLabel lblphone = new JLabel("Phone Number : ");
+        lblphone.setBounds(400,300,200,50);
+        lblphone.setFont(new Font("serif",Font.BOLD,20));
+        add(lblphone);
+
+        tfphone  = new JTextField();
+        tfphone.setBounds(600,310,150,30);
+        add(tfphone);
+
+        JLabel lblemail = new JLabel("Email :");
+        lblemail.setBounds(100,380,200,50);
+        lblemail.setFont(new Font("serif",Font.BOLD,20));
+        add(lblemail);
+
+        tfemail  = new JTextField();
+        tfemail.setBounds(200,390,150,30);
+        add(tfemail);
+
+        JLabel lblx = new JLabel("Class X(%) :");
+        lblx.setBounds(400,390,200,50);
+        lblx.setFont(new Font("serif",Font.BOLD,20));
+        add(lblx);
+
+        tfx  = new JTextField();
+        tfx.setBounds(600,400,150,30);
+        add(tfx);
+
+
+        JLabel lblx1 = new JLabel("Class XII(%) :");
+        lblx1.setBounds(100,470,200,50);
+        lblx1.setFont(new Font("serif",Font.BOLD,20));
+        add(lblx1);
+
+        tfx1  = new JTextField();
+        tfx1.setBounds(260,480,150,30);
+        add(tfx1);
+
+        JLabel lblStudentNumber = new JLabel("Student Number :");
+        lblStudentNumber.setBounds(450,470,200,50);
+        lblStudentNumber.setFont(new Font("serif",Font.BOLD,20));
+        add(lblStudentNumber);
+
+        tfSN  = new JTextField();
+        tfSN.setBounds(650,480,150,30);
+        add(tfSN);
+
+        JLabel lblcourse = new JLabel("Course :");
+        lblcourse.setBounds(100,550,200,50);
+        lblcourse.setFont(new Font("serif",Font.BOLD,20));
+        add(lblcourse);
+
+        String course[] = {"B.Tech","B.BA","B.CA","B.sc","M.sc","M.BA","M.CA","M.com","M.A"};
+        cbcourse  = new JComboBox(course);
+        cbcourse.setBounds(230,560,150,30);
+        cbcourse.setBackground(Color.WHITE);
+        add(cbcourse);
+
 
 
         setVisible(true);
